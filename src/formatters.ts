28258ea636e5929996435cea224451b6d0136603
Lucas Martins <lucas.mrt.dev@gmail.com>
2025-06-04 17:09:28 -0300

    feat: adiciona função para formatar strings de data ISO para strings de data no formato dd/mm/yyyy

---
+++ src/formatters.ts
@@ -0,0 +1,14 @@
+/**
+ * Formata uma string de data no padrão ISO8601 para o formato dd/mm/yyyy
+ * @param date String de data no padrão ISO8601 a ser formatada
+ * @returns String de data no formato dd/mm/yyyy
+ */
+export const formatISODateString = (date: string): string => {
+  const dateObj = new Date(date);
+
+  return new Intl.DateTimeFormat("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  }).format(dateObj)
+}
